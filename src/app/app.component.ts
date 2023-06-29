e4a4a57e0f7717024b2104627e18cd2e06e526c4
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2023-06-29 19:59:23 +0530

    add upload feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import {
   AlignmentType,
   Document,
@@ -17,6 +18,20 @@ import { saveAs } from 'file-saver';
 })
 export class AppComponent {
   title = 'wordPoC';
+  file: File | null = null;
+  myForm = new FormGroup({
+    wordDoc : new FormControl('')
+    
+  })
+  fileUpload($event: any){
+    const files = $event.target.files as FileList;
+    this.file = files.item(0)
+    console.log('Event', $event, this.file)
+  }
+openWord() {
+  console.log('Form Element',this.myForm.value)
+}
+
   docxDownload() {
     const doc = new Document({
       sections: [

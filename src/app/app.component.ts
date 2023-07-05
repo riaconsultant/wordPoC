0d661a5389b5bce809ba9e62db59166d9d6ea97d
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2023-07-05 19:41:12 +0530

    read csv file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import {
@@ -23,13 +24,31 @@ export class AppComponent {
     wordDoc : new FormControl('')
     
   })
+  userArray: any = [];
+  constructor(private http: HttpClient) {}
   fileUpload($event: any){
     const files = $event.target.files as FileList;
     this.file = files.item(0)
     console.log('Event', $event, this.file)
   }
 openWord() {
-  console.log('Form Element',this.myForm.value)
+  console.log('Form Element',this.myForm.value.wordDoc)
+  if(this.myForm.value.wordDoc) {
+    this.http.get('assets/data/Property-Cat-Template.csv', {responseType: 'text'})
+      .subscribe(
+          data => {
+              let csvToRowArray = data.split("\n");
+              for (let index = 1; index < csvToRowArray.length - 1; index++) {
+                let row = csvToRowArray[index].split(",");
+                this.userArray.push(row);
+              }
+              console.log(this.userArray);
+          },
+          error => {
+              console.log(error);
+          }
+      );
+  }
 }
 
   docxDownload() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {  } from 'docx';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {  } from 'docx';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
